22f203c1562b3a83bbf57b3fc8b78e8b19a35fd2
大野　佳樹 <yoshiki@oonoyosikinoAir.corega>
2025-06-01 13:32:00 +0900

    for cors

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Cookies from 'js-cookie'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://api.rbplus-rank-manager.site',
   withCredentials: true,
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
